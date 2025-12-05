0b1780ee8dd1122f86618aecb570522a0e2089db
FAROUK EL MAKHCHOUNE <135977755+Faroukelmak@users.noreply.github.com>
2025-12-05 11:32:20 +0100

    fixing bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1784,7 +1784,7 @@ const projectsDetailsContent = [
 Ce projet est une démonstration complète de mes compétences en **NLP, Fine-Tuning et MLOps**. Il consiste à développer un système de **Synthèse de Texte Abstraite** de pointe.
 </p>
 <ul>
-    <li>**Modèle :** Fine-tuning du modèle **PEGASUS** (`google/pegasus-cnn_dailymail`), un modèle SOTA **Seq2Seq** de type Transformer.</li>
+    <li><strong>Modèle :</strong> Fine-tuning du modèle <strong>PEGASUS</strong> (<code>google/pegasus-cnn_dailymail</code>), un modèle SOTA <strong>Seq2Seq</strong> de type Transformer.</li>
     <li>**Tâche Spécifique :** Spécialisation du modèle sur le dataset **SAMSum** pour le résumé de **conversations naturelles** (une tâche à haute valeur ajoutée).</li>
     <li>**Objectif MLOps :** Concevoir une **pipeline MLOps complète, dockerisée, modulaire** et déployée via une API **FastAPI** pour simuler un environnement de production.</li>
 </ul>
